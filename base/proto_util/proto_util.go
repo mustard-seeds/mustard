a30badb136920798ae8ff845fe24c516d144277d
Gao <glc@acbc3296317f.ant.amazon.com>
2016-01-21 20:19:00 +0800

    add proto utils

---
+++ base/proto_util/proto_util.go
@@ -0,0 +1,31 @@
+package proto_util
+
+import (
+	"mustard/base/log"
+	"mustard/internal/github.com/golang/protobuf/proto"
+)
+
+func Serialize(pb proto.Message) ([]byte, error) {
+	r,e := proto.Marshal(pb)
+	if e != nil {
+		log.Log.Error("MarShal failed")
+	}
+	return r,e
+}
+func Deserialize(s []byte, pb proto.Message) error {
+	e := proto.Unmarshal(s, pb)
+	if e != nil {
+		log.Log.Error("UnMarShal failed")
+	}
+	return e
+}
+func FromProtoToString(pb proto.Message) string {
+	return proto.MarshalTextString(pb)
+}
+func FromStringToProto(s string, pb proto.Message) error {
+	e := proto.UnmarshalText(s, pb)
+	if e != nil {
+		log.Log.Error("UnMarShal failed")
+	}
+	return e
+}
