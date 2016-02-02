5f1698d653a816746c539c9ce3c4898760f4ff48
gaolichuang <gaolichuang@gmail.com>
2016-02-02 22:06:37 +0800

    load file two field

---
+++ crawl/base/loadfile.go
@@ -0,0 +1,31 @@
+package base
+
+import (
+    "mustard/base/time_util"
+    "mustard/base/conf"
+    "mustard/base/file"
+    "strings"
+    LOG "mustard/base/log"
+)
+
+var CONF = conf.Conf
+var last_load_time int64
+
+func LoadConfigWithTwoField(name,filename,splitS string) (map[string]string,bool) {
+    if time_util.GetCurrentTimeStamp() - last_load_time < int64(*CONF.Crawler.ConfigFileReloadInterval) {
+        return nil,false
+    }
+    result := make(map[string]string)
+    last_load_time = time_util.GetCurrentTimeStamp()
+    LOG.Infof("Load Config %s",*CONF.Crawler.HostLoadConfigFile)
+    file.FileLineReader(filename, "#", func(line string){
+        addr :=strings.Split(line, splitS)
+        if len(addr) != 2 {
+            LOG.Errorf("%s Load Config Format Error, %s : %s", name, filename,line)
+            return
+        }
+        result[addr[0]] = addr[1]
+        LOG.VLog(4).Debugf("Load %s  %s : %s", name, addr[0], addr[1])
+    })
+    return result,true
+}
